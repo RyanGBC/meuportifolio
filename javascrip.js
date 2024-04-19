c761fd65c4e78f3399dcee4e2498cf3b71f70ae8
Ryan Gabriel Costa <ryangabrj@gmail.com>
2024-04-19 18:05:06 -0300

    removi o codigo de javascript

--- javascrip.js
+++ javascrip.js
@@ -1,14 +1,1 @@
-var ingredientes = ['Farinha', 'Açúcar', 'Ovos'];
 
-// Adiciona um novo ingrediente
-ingredientes.push('Leite');
-
-// Itera sobre os ingredientes
-ingredientes.forEach(function(ingrediente) {
-    console.log(ingrediente);
-});
-// Armazena uma receita no armazenamento local
-localStorage.setItem('receitaAtual', 'Bolo de Chocolate');
-
-// Recupera a receita do armazenamento local
-var receitaAtual = localStorage.getItem('receitaAtual');
